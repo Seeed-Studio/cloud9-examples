89b7c2e4497d6a1a74700f79e3b8382a9a3ff8b6
Zhangqun Ming <north_sea@qq.com>
2021-11-16 02:31:46 +0000

    bbai64: add all pwm CHs

--- BeagleBone/AI-64/pru/blinkR30.j7-pruX_Y.c
+++ BeagleBone/AI-64/pru/blinkR30.j7-pruX_Y.c
@@ -5,27 +5,30 @@
 #include <pru_ctrl.h>
 
 
+typedef struct {
+	uint32_t ctrl;
+	uint32_t duty;
+	uint32_t cycle;
+	uint32_t count;
+}pwm_t;
+
 volatile register unsigned int __R30;
 volatile register unsigned int __R31;
 
-#define MAXCH				1
-#define DELAY_1us			25
-#define CTRL_START			0x01
-#define CTRL_POLARY_L		0x02
+#define MAXCH			7
+#define DELAY_1US		25
+#define CTRL_START		0x01
+#define CTRL_POLARY_L	0x02
 
-#define SHL(n)				((uint32_t)0x01<<(n))
+#define SHL(n)			((uint32_t)0x01<<(n))
 
-volatile uint32_t *pwm = (uint32_t *)(0x10000);
-#define pwm_ctl(j)			pwm[j+0]
-#define pwm_dut(j)			pwm[j+1]
-#define pwm_cyc(j)			pwm[j+2]
-#define pwm_cnt(j)			pwm[j+3]
-
+#define PRU_SRAM __far __attribute__((cregister("PRU_SHAREDMEM", near)))
+PRU_SRAM volatile pwm_t pwm[MAXCH] = { 0 };
 
 int main(void)
 {
-	uint8_t i = 0, j = 0;
-	uint32_t pins[MAXCH] = { SHL(5), SHL(13), SHL(16), SHL(17), SHL(2), SHL(8), SHL(4) };
+	uint8_t i = 0;
+	uint32_t pins[MAXCH] = { 0 };//{ SHL(5), SHL(13), SHL(16), SHL(17), SHL(2), SHL(8), SHL(4) };
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
@@ -35,49 +38,49 @@ int main(void)
 
 	__R30 = 0x0;
 
-#pragma UNROLL(MAXCH)
+	pins[0] = SHL(5);
+	pins[1] = SHL(13);
+	pins[2] = SHL(16);
+	pins[3] = SHL(17);
+	pins[4] = SHL(2);
+	pins[5] = SHL(8);
+	pins[6] = SHL(4);
+
 	for (i=0; i<MAXCH; i++) {
-		j = i*4;
-		pwm_ctl(j) = CTRL_START; // ctrl
-		pwm_dut(j) = 1; // duty
-		pwm_cyc(j) = 2; // cycle
-		pwm_cnt(j) = 0; // count
+		pwm[i].ctrl = CTRL_START;
+		pwm[i].duty = 1;
+		pwm[i].cycle = 2;
+		pwm[i].count = 0x0;
 	}
 
 	for (;;) {
 		for (i=0; i<MAXCH; i++) {
-			j = i*4;
-			if (!(pwm_ctl(j) & CTRL_START)) {
-				if (pwm_ctl(j) & CTRL_POLARY_L) {
+			if (!(pwm[i].ctrl & CTRL_START)) {
+				if (pwm[i].ctrl & CTRL_POLARY_L)
 					__R30 |= pins[i];
-				}
-				else {
+				else
 					__R30 &= ~pins[i];
-				}
+			
 				continue;
 			}
 
-			if (pwm_cnt(j) >= pwm_cyc(j)) {
-				pwm_cnt(j) = 0;
+			if (pwm[i].count >= pwm[i].cycle) {
+				pwm[i].count = 0;
 			}
 
-			if (pwm_cnt(j) < pwm_dut(j)) {
-				if (pwm_ctl(j) & CTRL_POLARY_L) {
+			if (pwm[i].count < pwm[i].duty) {
+				if (pwm[i].ctrl & CTRL_POLARY_L)
 					__R30 &= ~pins[i];
-				}
-				else {
+				else
 					__R30 |= pins[i];
-				}
 			}
 			else {
-				if (pwm_ctl(j) & CTRL_POLARY_L) {
+				if (pwm[i].ctrl & CTRL_POLARY_L)
 					__R30 |= pins[i];
-				}
-				else {
+				else
 					__R30 &= ~pins[i];
-				}
 			}
-			pwm_cnt(j)++;
+			pwm[i].count++;
 		}
 	}
 
